b6f44b28e73393b05a4633f981e81db53503cc98
hamdanideni <carwall6@gmail.com>
2025-02-06 14:57:21 +0700

    front end mentor challenge - ping coming soon page

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const form = document.querySelector("#form");
+const emailInput = document.querySelector(".email");
+
+const isValidEmail = (input) => {
+  const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  return reg.test(String(input).toLowerCase());
+};
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const displayError = inputControl.querySelector(".error-message");
+
+  displayError.textContent = message;
+  inputControl.classList.add("error");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+
+  inputControl.classList.remove("error");
+  element.value = "";
+};
+
+const validateInputs = () => {
+  const inputValue = emailInput.value.trim();
+  console.log(inputValue);
+
+  if (inputValue === "") {
+    setError(emailInput, "Whoops! It looks like you forgot to add your email");
+  } else if (!isValidEmail(inputValue)) {
+    setError(emailInput, "Please provide a valid email addres");
+  } else {
+    setSuccess(emailInput);
+  }
+};
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  validateInputs();
+});
